124d4e50edfff71456bc96b95dbaeb94d2db63fd
VexAve <designdyle@gmail.com>
2026-09-13 15:05:31 +1000

    feat: add onChange prop to handle input changes

--- src/components/GeneralInformationSection.tsx
+++ src/components/GeneralInformationSection.tsx
@@ -4,17 +4,18 @@ import { camelToTitle } from "../utils";
 interface Props {
   generalInformation: GeneralInformation;
   isEditing: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>, key: string) => void;
 }
 
-function GeneralInformationSection({ generalInformation, isEditing }: Props) {
+function GeneralInformationSection({ generalInformation, isEditing, onChange }: Props) {
   return <>
     <dl>General Information</dl>
     {Object.entries(generalInformation).map(([key, value]) =>
       <>
-        <dt>{camelToTitle(key)}: </dt>
+        <dt>{camelToTitle(key)}:</dt>
         <dd>
           {isEditing
-            ? <input type="text" value={value} />
+            ? <input type="text" value={value} onChange={(e) => onChange(e, key)} />
             : value || 'N/A'
           }
         </dd>
